64d0d31dd3dafefbcaf7657702cd3a2caaed9694
Fuuun1024 <worapopfong@gmail.com>
2024-12-17 16:29:13 +0700

    10.2 complete

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -6,12 +6,21 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
 	
+	string textline;
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
-
+	cout << "-------------------- BOOM ---------------------\n";
+	//bool havetext;
+	//havetext = getline(source, texline);
+	while(getline(source, textline)){
+	    cout << textline << '\n';
+	}
+	
+	cout << "-------------------- HA!! ---------------------";
+    
     source.close();
     dest.close();
 	return 0;
